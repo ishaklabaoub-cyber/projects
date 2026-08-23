8807bbfaedbc0f937f45d406da40bca844d46056
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-08-23 12:51:17 +0100

    shell start

--- shell.c
+++ shell.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
+#define MAXTOKEN 1000
 
 int main()
 {
-	printf("Hello ,World!\n");
-	printf("Wow\n");
-	printf("Wow\n");
+	char token[MAXTOKEN];
+	do{
+		printf("My shell> ");
+		scanf("%999s",token);
+	}while( strcmp("exit",token) );
 	return 0;
 }
