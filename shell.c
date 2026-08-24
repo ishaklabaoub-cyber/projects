13227872531c737f262ea98dc0ee2062e46d4238
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-08-24 16:44:10 +0100

    adding a tokenizer

--- shell.c
+++ shell.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE    100
+#define TOK_BUFF_SIZE  64
+#define TOK_DELIM      ' \t\r\n\a'
 
-int   getch();
-void  ungetch(int);
-char* readlines();
-void  sh_loop(void);
+char** readtok(char*);
+char*  readlines();
+void   sh_loop(void);
 
 char *line;			/* line of commands */
+
 // SIGNALS
 int  signal_terminate = 0;
 
@@ -32,7 +34,7 @@ char* readline(){
 		exit(EXIT_FAILURE);
 	}
 	buf[0] = '\0';
-	
+
 	while(1){
 		c = getchar();
 		if(c == '\n'){
@@ -46,23 +48,55 @@ char* readline(){
 			bufp++;
 		}
 		if(bufp >= bufsize){				/* if the buffer position get exceeded , realloc*/
+			bufsize += BUFFER_SIZE;
 			buf = realloc(buf,sizeof(char) * BUFFER_SIZE);	
 			if(!buf){
 				fprintf(stderr,"realloc: failed allocation...\n");
 				exit(EXIT_FAILURE);
 			}		
-			bufsize += BUFFER_SIZE;
 		}
 	}
-
 }
+
+char** readtok(char* line)
+{
+	int    bufsize = TOK_BUFF_SIZE , position = 0;
+	char** tokens  = malloc(bufsize * sizeof(char)); 	/* Storing tokens to use them after */
+	char*  token;						/* Storing tokens from strtok() */
+
+	if( !tokens){
+		fprintf(stderr,"malloc : failed allocation...!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line,TOK_DELIM);
+	while( token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+	
+		if(position >= bufsize){			/* Checking for limits */
+			bufsize += TOK_BUFF_SIZE;
+			tokens = realloc(tokens,bufsize * sizeof(char));
+			if( !tokens){
+				fprintf(stderr, "malloc : failed allocation...!\n");
+				exit( EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL,TOK_DELIM);
+	}
+	tokens[position] = NULL;	
+	
+	return tokens;
+}
+
 void sh_loop()
 {
 	do{
 		printf("My shell> ");
 		line = readline();
 		
-		if(line == NULL){
+		if(signal_terminate == 1){
 			// EOF
 			return ;
 		}
