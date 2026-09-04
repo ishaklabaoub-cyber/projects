c83d4e693346d289ce9ac57716057b3b3a33da21
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-09-04 18:04:36 +0100

    some bugs needs to be fixed

--- simple-port-scanner/simple-port-scanner.c
+++ simple-port-scanner/simple-port-scanner.c
@@ -1,9 +1,72 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
+int is_valid_ipv4(const char*);
 
 
 int main()
 {
-	printf("Scanning...\n");
+	char input_addr[16];			/* to store the ip address from the user */
+	int port_min = 0, port_max = 65535;	/* port interval */
+	int socket_fd;				/* socket file descriptor */
+	struct sockaddr_in addr;		/* ip address */
+	
+	do{		// Getting IP address
+		printf("Please Enter IPv4 address that you want to scan: ");
+		scanf("%15s", input_addr);
+	
+	}while(is_valid_ipv4(input_addr) == 0);
+	
+	printf("%s is a valid IPv4 address\n",input_addr);
+	do{		// Getting port range
+		printf("\nUsage : <int>-<int> (example : 20-80)\n");
+		printf("Please Enter the range of ports: ");
+		scanf("%d-%d", &port_min, &port_max);
+
+	}while((port_min < 0 || port_max > 65535 ) && port_min > port_max);
+	
+	
+	for(int port = port_min; port <= port_max; ++port){
+		
+		socket_fd = socket(AF_INET,SOCK_STREAM,0);		/* creating an endpoint */
+		if(socket_fd == -1){
+			// socket() failed
+			perror("socket");
+			close(socket_fd);
+			return 1;
+		}
+
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(port);	
+		if( inet_pton(AF_INET, input_addr, &addr.sin_addr) != 1){
+			perror("inet_pton");
+			close(socket_fd);
+			return 1;
+		}
+
+		if( connect(socket_fd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) == -1){	/* trying to connect */
+			// connect() failed
+		 	perror("connect");
+			close(socket_fd);
+			return 1;
+		} else{
+			printf("port %d is open on %s\n\n", port, input_addr);
+		}
+		
+	}
+	close(socket_fd);
+
+	return 0;
+}
+
+int is_valid_ipv4(const char *src)
+{
+	struct sockaddr_in dst;
+
+	if(inet_pton(AF_INET, src, &dst) == 1){
+		return 1;
+	}
 	return 0;
 }
