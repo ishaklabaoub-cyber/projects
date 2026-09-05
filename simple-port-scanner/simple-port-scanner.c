56c9ca4b1dd4fbbf76e7908ee5c91990f785c8f2
Labaoub Mohamed Ishak <ishaklabaoub@gmail.com>
2026-09-05 16:32:52 +0100

    90% completed

--- simple-port-scanner/simple-port-scanner.c
+++ simple-port-scanner/simple-port-scanner.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <sys/poll.h>
 
 int is_valid_ipv4(const char*);
@@ -13,7 +16,10 @@ int main()
 	int port_min = 0, port_max = 65535;	/* port interval */
 	int socket_fd;				/* socket file descriptor */
 	struct sockaddr_in addr;		/* ip address */
-	struct pollfd pfd;
+	struct pollfd pfd;			/* for a non-blocking socket */
+	int    expected_error = EINPROGRESS;
+	int    err;
+	socklen_t len = sizeof(err);
 	
 	do{		// Getting IP address
 		printf("Please Enter IPv4 address that you want to scan: ");
@@ -31,7 +37,7 @@ int main()
 			port_max = 65536;
 		}
 
-	}while((port_min < 0 && port_max > 65535 ) || port_min > port_max);
+	}while(port_min < 0 || port_max > 65535  || port_min > port_max);
 	
 
 	for(int port = port_min; port <= port_max; ++port){
@@ -54,22 +60,59 @@ int main()
 		}
 
 		/***************** must add the poll() function for non-blocking sockets*******************/ 
+		fcntl(socket_fd, F_SETFL, O_NONBLOCK);
 		pfd.fd = socket_fd;
 		pfd.events = POLLOUT;
 		pfd.revents = 0;
-
-		if( connect(socket_fd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) == -1){	/* trying to connect */
-			// connect() failed
-		 	perror("connect");
-			close(socket_fd);
-		} else{
-			poll(&pfd, 1, 1000);
-			if(pfd.revents & POLLOUT){
-				printf("port %d is open on %s\n\n", port, input_addr);
-			}
-			close(socket_fd);
-		}
 		
+		errno = 0;
+		int ret = connect(socket_fd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in));		/* trying to connect */
+		if(ret == -1){
+			int current_error = errno;
+			if(expected_error == current_error){	// connection in progress
+
+				int return_val_poll = poll(&pfd, 1, 300);
+				if(pfd.revents & POLLOUT){	// the socket is writable
+
+					if(getsockopt(socket_fd, SOL_SOCKET, SO_ERROR, &err, &len) == -1){
+						// getsockopt() failed
+						perror("getsockopt");
+					} else{
+						if(err == 0){	// connection succeeded
+							printf("port %d is open on %s\n", port, input_addr);
+
+						} else if(err == ECONNREFUSED){ // connection refused
+							
+							printf("port %d is closed on %s\n", port, input_addr);
+
+						} else if(err == ETIMEDOUT){	// connection timed out
+							
+							printf("connection timed out on port %d .\n", port);
+						} else if(err == ENETUNREACH){  // network unreachable
+							
+							printf("network is unreachable.\n");
+						} else{
+							printf("some error\n");			/*****************************FIIIIIIIIIIIIIIIIIXXXXXXXXXXX**********/
+						}
+					}
+
+				} else if(pfd.revents & POLLERR){	//check if any errors occured in pfd.revents
+					
+					printf("error occured.\n");
+				
+				} else if(return_val_poll == 0){	// the poll timeout ran out of time
+					printf("connection timeout.\n");
+				}
+			} else if(current_error == ECONNREFUSED){
+				printf("port %d is closed on %s\n", port, input_addr);
+			} else{
+				perror("connect");
+			}		
+		} else{
+			// connection succeed
+			printf("port %d is open on %s\n", port, input_addr);
+		}
+			close(socket_fd);
 	}
 
 	return 0;
